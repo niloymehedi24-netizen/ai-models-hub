922620522223cdde6b98814caf50db320b0b335f
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-04-07 16:34:35 +0600

    added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+
       <Banner></Banner>
+
+      <Footer></Footer>
     </>
   );
 }
