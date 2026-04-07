75cf64830c641b0e3e3d5af1d86583e12a70891c
hasanniloy122-rgb <hasanniloy122@gmail.com>
2026-04-07 16:29:14 +0600

    added banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Banner from "./components/Banner";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
+      <Banner></Banner>
     </>
   );
 }
